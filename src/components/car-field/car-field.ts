13d30d19e9293b9bd3926acf627647cd0a904b1f
Vadavur <vadavur@gmail.com>
2021-06-18 01:20:00 +0300

    refactor: implement svg from file insertion

--- src/components/car-field/car-field.ts
+++ src/components/car-field/car-field.ts
@@ -1,17 +1,21 @@
 import './car-field.scss';
 import { BaseComponent } from '../shared/base-component';
-import { CAR_SVG_VECTOR } from '../shared/constants';
+import { INLINE_SVG_CARBODY_ID } from '../shared/constants';
 
 export class CarField extends BaseComponent {
   constructor(carColor: string, carId: number) {
     super('div', ['car-field']);
-    this.element.innerHTML = CAR_SVG_VECTOR;
-    this.setCarView(carColor, carId);
+    fetch('/public/car.svg')
+      .then((res) => res.text())
+      .then((SVGText) => {
+        this.element.innerHTML = SVGText;
+        this.setCarView(carColor, carId);
+      });
   }
 
   setCarView(carColor: string, carId: number): void {
     this.element.setAttribute('data-before', carId.toString());
-    const carBody = this.element.querySelector('#car-body');
+    const carBody = this.element.querySelector(`#${INLINE_SVG_CARBODY_ID}`);
     (carBody as HTMLElement).style.fill = carColor;
   }
 }
